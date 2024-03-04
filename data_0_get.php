b7284051f669ca1f2df3407e86144f608258d424
JustaWhatever <83577810+an-alch3mist@users.noreply.github.com>
2024-03-05 01:39:26 +0600

    Add files via upload
    button search : info of progress
    
    flex inside flex
    	space around property
    
    
    css child navigation : >

---
+++ data_0_get.php
@@ -0,0 +1,84 @@
+
+<?php
+	$server = "localhost";
+	$user = "root";
+	$pass = ' ';
+	$db = "db-00-user-name-max";
+
+	// Create connection
+	$cnc = new mysqli($server, $user, $pass, $db);
+?>
+
+
+
+
+
+<link rel="icon" href="fav32.png">
+<body style = "background: #222">
+<pre  style = "color: #eee;">
+<~><?php
+
+/* 
+req - request	- i , u , s
+val - value		- TXT  	(feed for i)
+id  - id		- int	(feed for u)
+*/
+
+
+$req = $_GET["req"];
+
+if($req == "i")
+{
+
+	$name = $_POST["name"];
+	$val = $_POST["val"];
+
+	$str = <<<HerNow
+	INSERT INTO `table_0`(`value` , `name`) 
+	VALUES ("$val" , "$name")
+	HerNow;
+
+	$cnc->query($str);
+
+	// echo $str; 
+}
+else if($req == "u")
+{
+	$id = $_POST["id"];
+	$name = $_POST["name"];
+	$val = $_POST["val"];
+
+	$str = <<<HerNow
+	UPDATE `table_0` SET `value`= "$val and `name` = "$name" 
+	WHERE `id`= $id
+	HerNow;
+
+	$cnc->query($str);
+
+	// echo $str; 
+}
+else if($req == "s")
+{
+
+	$str = <<<HerNow
+	SELECT * 
+	FROM `table_0`
+	HerNow;
+
+	$result = $cnc->query($str);
+	//
+	while($row = $result->fetch_assoc()) 
+	{
+    	echo $row["value"]. "~";
+  	}
+  	echo "end-of-select";
+  	
+	// echo $str; 
+}
+
+
+
+$cnc->close();
+
+
+
